aabb8878ecdc24638ef1284663d4eb4cc694851f
taemin hwang <integralhtm@gmail.com>
2023-08-14 17:55:44 +0900

    merge file

--- frontend/project/src/views/MatchingApplyDetailPage.vue.vue
+++ frontend/project/src/views/MatchingApplyDetailPage.vue.vue
@@ -54,19 +54,147 @@
     </nav>
 
     <div class="container02">
-  <button type="button" id="btnMyPosts">내 게시글</button>
-  <button type="button" id="btnMyComments">내 댓글</button>
-</div>
+    <button @click="showPosts" :class="{ 'active': isShowingPosts }">내 게시글</button>
+    <button @click="showComments" :class="{ 'active': !isShowingPosts, 'purple-button': !isShowingPosts }">내 댓글</button>
+  </div>
+  
+
+    <!-- 댓글 데이터 표시 -->
+    <div class="comment-table" v-if="!isShowingPosts">
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">내용</th>
+            <th scope="col">작성일</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(comment, index) in myComments" :key="index">
+            <th scope="row">{{ index + 1 }}</th>
+            <td>{{ comment.text }}</td>
+            <td>{{ comment.date }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
 
     <!-- 매칭 현황 -->
     <div class="match-status">
-      <!-- 매칭 현황의 내용이 동적으로 표시될 영역 -->
+      <div v-if="isShowingPosts">
+        <div v-for="(post, index) in myPosts" :key="index" class="post-it">
+          <h3>{{ post.title }}</h3>
+          <p>내용: {{ post.content }}</p>
+        </div>
+      </div>
+      <div v-else>
+        <div v-for="(comment, index) in myComments" :key="index" class="post-it">
+          <h3>{{ comment.postTitle }}</h3>
+          <p>내 댓글: {{ comment.text }}</p>
+        </div>
+      </div>
     </div>
+
+    <div class="content" style="margin-top: -100px;">
+  <section id="postItsContainer" style="margin-top: -100px;" v-if="isShowingPosts">
+    <div v-for="(post, index) in posts" :key="index" :class="['post-it', post.gender]">
+      <h3>{{ post.title }}</h3>
+      <p>인원: {{ post.members }}</p>
+      <p>학과: {{ post.department }}</p>
+      <p>내용: {{ post.details }}</p>
+    </div>
+  </section>
+</div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      isShowingPosts: true, // 기본적으로 게시글 표시
+      myComments: [
+        // 여기에 댓글 데이터를 가져와서 할당하는 로직을 추가해야 합니다.
+        // 예를 들어, 서버에서 댓글 데이터를 가져와서 할당하는 코드를 추가하세요.
+        // 예시 데이터:
+        {
+          text: "첫 번째 댓글",
+          date: "2023-08-13",
+        },
+        {
+          text: "두 번째 댓글",
+          date: "2023-08-14",
+        },
+        // ... 댓글 데이터 추가 ...
+      ],
+
+      // ... (other data properties) ...
+      posts: [
+        {
+          title: '포스트잇 제목 1',
+          members: '2명',
+          department: '공과대학 시스템경영공학부 산업경영공학전공',
+          details: '매칭 상세 내용 1...',
+          gender: 'male'
+        },
+        {
+          title: '포스트잇 제목 1',
+          members: '2명',
+          department: '공과대학 시스템경영공학부 산업경영공학전공',
+          details: '매칭 상세 내용 1...',
+          gender: 'male'
+        },
+        {
+          title: '포스트잇 제목 1',
+          members: '2명',
+          department: '공과대학 시스템경영공학부 산업경영공학전공',
+          details: '매칭 상세 내용 1...',
+          gender: 'male'
+        },
+        {
+          title: '포스트잇 제목 1',
+          members: '2명',
+          department: '공과대학 시스템경영공학부 산업경영공학전공',
+          details: '매칭 상세 내용 1...',
+          gender: 'male'
+        },
+        {
+          title: '포스트잇 제목 1',
+          members: '2명',
+          department: '공과대학 시스템경영공학부 산업경영공학전공',
+          details: '매칭 상세 내용 1...',
+          gender: 'male'
+        },
+        {
+          title: '포스트잇 제목 1',
+          members: '2명',
+          department: '공과대학 시스템경영공학부 산업경영공학전공',
+          details: '매칭 상세 내용 1...',
+          gender: 'male'
+        },
+        {
+          title: '포스트잇 제목 1',
+          members: '2명',
+          department: '공과대학 시스템경영공학부 산업경영공학전공',
+          details: '매칭 상세 내용 1...',
+          gender: 'male'
+        },
+        {
+          title: '포스트잇 제목 1',
+          members: '2명',
+          department: '공과대학 시스템경영공학부 산업경영공학전공',
+          details: '매칭 상세 내용 1...',
+          gender: 'male'
+        },
+        {
+          title: '포스트잇 제목 1',
+          members: '2명',
+          department: '공과대학 시스템경영공학부 산업경영공학전공',
+          details: '매칭 상세 내용 1...',
+          gender: 'male'
+        },
+        // ... (add more posts as needed) ...
+      ],
   methods: {
     // 내 정보 페이지로 이동
     goToMyInfoPage() {
@@ -95,7 +223,17 @@ export default {
     goToNoticePage() {
       this.$router.push("/NoticePage");
     },
-  }
+
+    showPosts() {
+      this.isShowingPosts = true;
+    },
+    
+    showComments() {
+      this.isShowingPosts = false;
+    }
+  },
+};
+},
 }
 </script>
 
@@ -201,8 +339,13 @@ body {
     color: white; /* 버튼 텍스트 색상 흰색으로 설정 */
     font-size: 16px;
     transition: background-color 0.3s; /* 호버 효과 추가 */
+    cursor: pointer; /* 커서를 손가락 모양으로 변경하여 클릭 가능한 요소임을 나타냄 */
 }
-
+.basicMenu button.active {
+  background-color: #9370db;
+  /* 기존 스타일 유지 */
+  /* ... */
+}
 .basicMenu button:hover {
     background-color:#cccccc; /* 호버시 진한 회색으로 변경 */
 }
@@ -211,16 +354,12 @@ body {
   top: 95px; /* Adjust this value according to your fixed navbar height */
 }
 
-.container {
-  display: flex;
-  justify-content: center;
-}
-
 .container02 {
   display: flex;
   justify-content: center; /* 수평 중앙 정렬 */
   align-items: center; /* 수직 중앙 정렬 */
   height: 40vh; /* 화면 전체 높이 */
+  margin-left: 100px;
 }
 
 /* 버튼 스타일링 (옵션) */
@@ -245,6 +384,71 @@ body {
   margin: 5px;
 }
 
+/* 활성 버튼 스타일 */
+.container02 button.active {
+  background-color: #9370db;
+  color: white;
+}
+
+.post-it {
+  width: 250px; /* Set the width to create a square */
+  height: 250px; /* Set the height to match the width */
+  margin: 10px;
+  padding: 10px;
+  box-shadow: 2px 2px 5px #aaa;
+  background-color: #fefabc; /* Default yellow color for post-it */
+  transition: transform 0.2s; /* Animation for hover effect */
+}
+.post-it:hover {
+    transform: scale(1.05); /* Slightly enlarge the post-it on hover */
+}
+
+.post-it.male {
+    background-color: #deeeff; /* Blue color for male */
+}
+
+.post-it.female {
+    background-color: #ffe4ee; /* Pink color for female */
+}
+
+#postItsContainer {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
+  justify-items: center;
+  margin-left: 200px;
+  margin-bottom: 450px;
+  transition: margin-left 0.3s;
+}
+
+.board-list .btn {
+    margin-top: auto;
+    background-color: #F6E6F6;
+    color: #9370DB; /* 버튼 텍스트 색상 흰색으로 설정 */
+    border-color: #F6E6F6;
+    width: 100%;
+}
+
+.board-list .btn:hover {
+    transform: scale(1.05); 
+}
+
+
+.content {
+  display: flex;
+  flex-direction: column;
+  padding-top: 100px;
+  padding-left: 200px;
+  margin: 20px 0;
+  border: none; /* 테두리 제거 */
+  
+} 
+
+.purple-button {
+  background-color: #9370DB; /* 보라색 배경색 */
+  color: white; /* 흰색 텍스트 색상 */
+  transition: background-color 0.3s, color 0.3s; /* 호버 및 토글 애니메이션 적용 */
+}
 
 
 @import "~bootstrap/dist/css/bootstrap.min.css";
--- frontend/project/src/views/MatchingBoardDetailPage.vue
+++ frontend/project/src/views/MatchingBoardDetailPage.vue
@@ -46,12 +46,42 @@
       </div>
     </nav>
     </div>
+    
+<div class="content">
+      <section id="postDetailContainer">
+          <div class="post-it male">
+              <h3>포스트잇 제목</h3>
+              <p>인원: 2명</p>
+              <p>학과: 공과대학 시스템경영공학부 산업경영공학전공</p>
+              <p>내용: 매칭 상세 내용...</p>
+          </div>
+          
+          <div class="comments-section">
+        <!-- <h4>댓글</h4> -->
+        <div class="comments-list">
+          <ul class="comment-ul">
+            <li v-for="comment in comments" :key="comment">{{ comment }}</li>
+          </ul>
+        </div>
+
+        <div class="add-comment">
+          <textarea v-model="newComment" placeholder="댓글을 작성해주세요."></textarea>
+          <button class="comment-button" @click="addComment">댓글 추가</button>
+        </div>
+
+        <p class="warning-text" v-if="isCommentEmpty">추가할 내용이 없습니다.</p>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
   return {
+    comments: ['댓글1: 안녕하세요! 매칭을 원합니다.', '댓글2: 저도 참여하고 싶네요!'],
+      newComment: '',
+      isCommentEmpty: false // 추가된 부분
   };
 },
   methods: {
@@ -76,143 +106,224 @@ export default {
 
     goToNoticePage() {
       this.$router.push("/NoticePage");
-    },
+    },addComment() {
+      if (this.newComment.trim() !== '') {
+        this.comments.push(this.newComment);
+        this.newComment = '';
+        this.isCommentEmpty = false; // 내용이 있을 때 경고 문구 제거
+      } else {
+        this.isCommentEmpty = true; // 내용이 없을 때 경고 문구 표시
+      }
+    }
   }
 }
 </script>
 
 <style scoped>
 body {
-  margin: 0;
-  background-color: white;
+    margin: 0;
+    background-color: white;
 }
 
 .MatchingUs {
-  font-size: 28px;
-  color: #9370DB;
-}
-
-/* 이미지의 크기를 조절하는 .img 클래스에 스타일 추가 */
-.img {
-  width: 200px; /* 이미지의 너비를 200px로 조정 */
-  height: auto; /* 높이를 자동으로 조정하여 가로 세로 비율 유지 */
-  margin-left: -20px; /* 이미지를 왼쪽으로 20px 이동 */
+    font-size: 24px; /* 로고 사이즈를 24px로 조정 */
+    color: #9370DB;
 }
 
 .navbar {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: #f6e6f6;
-  padding: 8px 12px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #F6E6F6;
+    padding: 8px 12px;
 }
 
 .navbar_logo {
-  font-size: 24px;
+    font-size: 24px;
 }
 
 .navbar_logo .logo_image {
-  margin-right: 8px; 
+    margin-right: 8px; 
 }
 
 .navbar_welcome {
-  display: flex;
-  align-items: center;
-  margin-right: 30px; 
-  margin-top: 10px;
-  font-weight: 400;
+    display: flex;
+    align-items: center;
+    margin-right: 30px; 
+    margin-top: 10px;
+    font-weight: 400;
 }
 
 .navbar_welcome .welcome {
-  color: #9370DB;
-  margin-bottom: 10px;
+    color: #9370DB;
+    margin-bottom: 10px;
 }
 
 .btn.btn-outline-success {
-  /* color: #9370DB; */
-  border-color: #9370DB; 
-  font-weight: 500;
+    /* color: #9370DB; */
+    border-color: #9370DB; 
+    font-weight: 500;
 }
 
 .navbar_right {
-  display: flex;
-  align-items: center;
+    display: flex;
+    align-items: center;
 }
 
 .navbar_right .btn {
-  margin-left: auto;
-  background-color: #f6e6f6; /* Set the background color to match the navbar background color */
+    margin-left: auto;
+    background-color: #F6E6F6; /* Set the background color to match the navbar background color */
 /* Set the border color to match the text color */
 }
 
 .navbar_right .btn a {
 /* Set the text color of the button */
-  text-decoration: none;
+    text-decoration: none;
 }
 
 .navbar_right .btn:hover {
-  transform: scale(1.05);
+    transform: scale(1.05);
 }
+
+
 
 /* 사이드 메뉴 바 */
 .side_nav {
-  width: 200px;
-  height: 100%;
-  position: fixed;
-  background-color: #e0e0e0; /* 회색으로 변경 */
-  padding: 20px;
+    width: 200px;
+    height: 100%;
+    position: fixed;
+    background-color:#E0E0E0; /* 회색으로 변경 */
+    padding: 20px;
 }
 
 .basicMenu {
-  list-style: none;
-  padding: 0;
-  margin: 0;
+    list-style: none;
+    padding: 0;
+    margin: 0;
 }
 
 .basicMenu button {
-  background: none;
-  border: none;
-  cursor: pointer;
-  padding: 10px;
-  margin-bottom: 10px;
-  width: 100%;
-  text-align: left;
-  color: white; /* 버튼 텍스트 색상 흰색으로 설정 */
-  font-size: 16px;
-  transition: background-color 0.3s; /* 호버 효과 추가 */
+    background: none;
+    border: none;
+    cursor: pointer;
+    padding: 10px;
+    margin-bottom: 10px;
+    width: 100%;
+    text-align: left;
+    color: white; /* 버튼 텍스트 색상 흰색으로 설정 */
+    font-size: 16px;
+    transition: background-color 0.3s; /* 호버 효과 추가 */
 }
 
 .basicMenu button:hover {
-  background-color: #cccccc; /* 호버시 진한 회색으로 변경 */
+    background-color:#cccccc; /* 호버시 진한 회색으로 변경 */
 }
 
-.menuBar {
-  top: 95px; /* Adjust this value according to your fixed navbar height */
-}
-
-.container {
+.content {
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  padding-top: 100px;
+  padding-left: 350px;
+  margin: 20px 0;
+  border: none; /* 테두리 제거 */
 }
 
-.match-status {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center; /* 변경: 중앙 정렬로 수정 */
-  align-items: flex-start;
+/* 상세 정보 스타일 */
+
+.post-it.male {
+  background-color: #deeeff; /* Blue color for male */
+}
+
+.post-it.female {
+  background-color: #ffe4ee; /* Pink color for female */
+
+}
+.post-it {
+  width: 1000px;
+  height: 500px;
+  margin: 10px;
+  padding: 10px;
+
+
+  box-shadow: 2px 2px 5px #aaa;
+  background-color: #fefabc; /* Default yellow color for post-it */
+  transition: transform 0.2s; /* Animation for hover effect */
+}
+
+
+
+/* 댓글 섹션 스타일 */
+
+.comment-ul {
+  list-style: none;
+  margin: none;
+  padding: 10px;
+}
+
+.comment-ul li {
+  margin-bottom: 10px; /* 댓글 간격을 조정 */
+}
+
+.comment-button {
+  width: 70px;
+  
+}
+
+.comments-section {
+  width: 1000px;
+  border: 1px solid #eee;
+  border-radius: 5px;
+  margin: 10px;
+  margin-top: 30px;
+  padding: 10px;
+}
+
+.add-comment {
+  display: flex; /* Flexbox를 활성화합니다 */
+  justify-content: space-between; /* 아이템들 사이에 공간을 균등하게 배분합니다 */
   margin-top: 20px;
-  margin-left: 70px;
-  margin-right: 70px; /* 좌우 여백을 동일하게 설정 */
+  height: 70px;
 }
 
+.add-comment textarea {
+  flex: 1; /* textarea가 가능한 한 많은 공간을 차지하게 합니다 */
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #eee;
+  border-radius: 5px;
+  margin-bottom: 10px;
+}
 
+.add-comment button {
+  width: 100px; /* 버튼의 너비 */
+  height: 60px; 
+  background-color: #F6E6F6;
+  color: #9370DB;
+  border: 1px solid #9370DB;
+  padding: 7px;
+  border-radius: 15px;
+  cursor: pointer;
+  font-size: 14px;
+  margin-bottom: 8px;
+}
+
+.warning-text {
+  color: red;
+  font-size: 15px;
+  margin-top: 10px;
+  font-weight: bold;
+}
+/* 반응형 레이아웃 (사이드바가 사라지는 화면 크기 이하) */
+@media (max-width: 768px) { /* 예시: 768px 이하의 화면 크기에서 적용 */
+    .content {
+        margin-left: 20px; /* 사이드바가 사라지면 좌측 마진을 줄임 */
+    }
+    
+    .side_nav {
+        display: none; /* 사이드바를 숨김 */
+    }
+}
 
 @import "~bootstrap/dist/css/bootstrap.min.css";
 @import "~@fortawesome/fontawesome-free/css/all.min.css";
-
-@media screen and (max-width: 768px) {
-    .side_nav{
-      display: none;
-    }
-}
 </style>
